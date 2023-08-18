99a2f21460f1379c09d4d3fc495a182b32588702
Jean <124012241+Sotogamer19@users.noreply.github.com>
2023-08-18 11:27:02 -0600

    Pagos en claseAgenda
    Después de la Agenda, se añadió las funciones que sirven para saber el pago de los barberos, dependiendo de si es finde o no, esto se logra con la implementación de la librería Calendar.

---
+++ ClaseAgenda.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_barbero_final;
+
+/**
+ *
+ * @author jeanc
+ */
+public class ClaseAgenda {
+    
+}
